6cb3b4b66ab32852cb1fec5e0c55336cfad72d27
ebanos <edalbadi@gmail.com>
2024-10-02 10:39:28 -0600

    Updated Not Found Component Styles

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../styles/NotFound.module.scss";
+import "../styles/NotFound.scss";
 const NotFound: React.FC = () => {
   return (
     <>
       <div className="container">
-        <h1>404</h1>
+        <h1 className="header-error">404</h1>
         <h2>¡Ups! Página no encontrada</h2>
         <p>
           Lo sentimos, pero la página que buscas no está disponible o ha sido
@@ -14,13 +14,6 @@ const NotFound: React.FC = () => {
         <Link to="/login" className="home-button">
           Volver al Inicio
         </Link>
-
-        <div className="image-container">
-          <img
-            src="https://cdn-icons-png.flaticon.com/512/2748/2748558.png"
-            alt="Error 404"
-          />
-        </div>
       </div>
     </>
   );
